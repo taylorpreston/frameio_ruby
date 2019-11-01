f721150e468fa9af7f506862ba1a1adb65a2205e
Taylor Preston <taylor.preston.music@gmail.com>
2019-11-01 11:10:51 -0500

    Added presentations and modified app installs

--- lib/resources/app_installs.rb
+++ lib/resources/app_installs.rb
@@ -1,5 +1,5 @@
 module Frameio
-  class Users < APIResource
+  class AppInstalls < APIResource
     # @param frame_token [FrameioAuthToken] Auth token retrieved from Frame.io
     def self.find(frameio_token:)
       request(:get, "#{BASE_URL}/installations/for_me", authorize_bearer_header(frameio_token))
